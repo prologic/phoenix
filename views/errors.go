fba0605f26febc17b4554aa3d73a18c7258910e7
James Mills <james@mills.io>
2024-03-23 08:30:30 +1000

    Log errors

--- views/errors.go
+++ views/errors.go
@@ -1,11 +1,14 @@
 package views
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func ShowError(c *gin.Context, err error) {
+	logrus.WithError(err).Error()
 	c.HTML(
 		http.StatusBadRequest,
 		"error.html.tmpl",
